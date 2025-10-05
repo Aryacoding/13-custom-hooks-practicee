a439b786070c17f1a51698d4e96b3757b32a1ada
Aryanokouji <ariabima729@gmail.com>
2025-10-05 20:54:33 +0700

    Initial commit

---
+++ src/components/MoviesList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import useMovies from '../hooks/useMovies';
+
+function MoviesList() {
+  const [movies, loading] = useMovies();
+
+  React.useEffect(() => {
+    getMovies().then((movies) => {
+      setMovies(movies);
+      setLoading(false);
+    });
+
+    return () => {
+      setLoading(true);
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="movies-list">
+        <p>Loading ....</p>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="movies-list">
+      {movies.map((movie) => {
+        return (
+          <li key={movie.id} className="movies-list-item">
+            <h1>{movie.title}</h1>
+            <p>{movie.overview}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default MoviesList;
